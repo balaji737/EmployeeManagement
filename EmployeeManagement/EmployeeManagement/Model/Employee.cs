0e23cb051260ba50b5febb14941ff7375362e85c
BalajiPrasad Marni <marnibalajiprasad55@gmail.com>
2023-12-13 11:59:20 +0530

    Modified some changes

--- EmployeeManagement/EmployeeManagement/Model/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Model/Employee.cs
@@ -1,38 +1,48 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManagement.Model
 {
     public class Employee
     {
-        [Required(ErrorMessage = "Id is required.")]
+        [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
+        [Required(ErrorMessage = "Name is required."), Column(TypeName = "Varchar(25)")]
         [StringLength(100, ErrorMessage = "Name should not execeed 100 characters.")]
         public string Name { get; set; }
 
-        [Range(0, 10000000, ErrorMessage = "Salary should be non-negative number")]
+        [Required(ErrorMessage = "Salary should be non-negative number.")]
+        [Range(0, 10000000)]
         public int Salary { get; set; }
 
-        [Required(ErrorMessage = "Location is required.")]
+        [Required(ErrorMessage = "Location is required."), Column(TypeName = "Varchar(25)")]
         public string Location { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
+        [Required(ErrorMessage = "Email is required."), Column(TypeName = "Varchar(25)")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Department is required.")]
+        [Required(ErrorMessage = "Department is required."), Column(TypeName = "Varchar(25)")]
         [StringLength(50, ErrorMessage = "Department should not execeed 50 characters.")]
         public string Department { get; set; }
 
-        [Required(ErrorMessage = "Qualification is required.")]
+        [Required(ErrorMessage = "Qualification is required."), Column(TypeName = "Varchar(25)")]
         [StringLength(100, ErrorMessage = "Qualification should not exceed 100 characters.")]
         public string Qualification { get; set; }
 
-        [Required(ErrorMessage = "CreatedDate is required.")]
-        public DateTime CreatedDate { get; set; }
-
-        [Required(ErrorMessage = "UpdatedDate is required.")]
-        public DateTime UpdatedDate { get; set; }
+        DateTime _createdDate;
+        public DateTime? CreatedDate
+        {
+            set 
+            {
+                _createdDate = DateTime.Now;
+            }
+            get
+            {
+                return _createdDate;
+            }
+        }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
